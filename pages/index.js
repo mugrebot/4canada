beb80e61091ae734657ee418a42e80878bba5e62
Mugrebot <mikeongay@icloud.com>
2022-08-10 16:33:55 -0400

    yeet

--- pages/index.js
+++ pages/index.js
@@ -23,12 +23,15 @@ export default function Beep() {
 <h2>INFO@FIRESTARTER.COM</h2>
 
 <div className="Yeeters">
+  <div className="Column">
 <Image  height={'100px'} width={'100px'} src={"/bnr.png"} alt={'not found'}/>
-
+</div>
+<div className="Column">
 <Image height={'100px'} width={'100px'} src={"/UKU.png"} alt={'not found'}/>
-
+</div>
+<div className="Column">
 <Image height={'100px'} width={'100px'} src={"/HC.png"} alt={'not found'}/>
-
+</div>
 </div>
 
 
